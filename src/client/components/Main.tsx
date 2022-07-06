3169a374abbfdd22aeea1877f97a29dbe5fc2877
JSEvgeny <linc-sk8@mail.ru>
2022-07-06 21:07:47 +0300

    Adjust client. Create entrypoint App

--- src/client/components/Main.tsx
+++ src/client/components/Main.tsx
@@ -1,15 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react'
 
 const Main = () => {
-    const [cards, setCards] = useState([])
+    const [hand1, setHand1] = useState([])
+    const [hand2, setHand2] = useState([])
+    const [rest, setRest] = useState([])
 
     const fetchData = useCallback(async () => {
         // get the data from the api
         const data = await fetch('http://localhost:3000/stack')
         // convert data to json
         const json = await data.json()
+        const {hand1, hand2, rest} = json
 
-        setCards(json)
+        setHand1(hand1)
+        setHand2(hand2)
+        setRest(rest)
     }, [])
 
     useEffect(() => {
@@ -18,9 +23,28 @@ const Main = () => {
 
     return (
         <>
-            <h1>You hello!</h1>
+            <h1>Welcome to Durak game!</h1>
 
-            {cards.length > 0 && cards.map((card) => <span>{card}</span>)}
+            {hand1.length > 0 && (
+                <>
+                <h2>Player one has: </h2>
+                {hand1.map((card, index) => <span key={`hand_one_key_${index}`}>{card}</span>)}
+                </>
+            )}
+
+            {hand2.length > 0 && (
+                <>
+                <h2>Player two has: </h2>
+                {hand2.map((card, index) => <span key={`hand_two_key_${index}`}>{card}</span>)}
+                </>
+            )}
+
+            {rest.length > 0 && (
+                <>
+                <h2>Left in stack: </h2>
+                {rest.map((card, index) => <span key={`rest_key_${index}`}>{card}</span>)}
+                </>
+            )}
         </>
     )
 }
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Main from './components/Main'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <>
-        <Main />
+        <App />
     </>
 )
